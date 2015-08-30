f12067d05b1da1ea3805f72abbae2f4a9a357c18
Ismail Syed <isyed867@gmail.com>
2015-08-30 17:10:39 -0400

    added form validation logic

--- client/application.js
+++ client/application.js
@@ -23,23 +23,36 @@
 
  Template.userInterface.events({
     "submit #submitRequest": function(e){
-
       e.preventDefault();
-
       var requestObj = {};
       requestObj.firstName = e.target.firstName.value; 
       requestObj.lastName = e.target.lastName.value; 
       requestObj.email = e.target.email.value; 
       requestObj.studentNumber = e.target.studentNumber.value; 
-      requestObj.coursepack = e.target.coursepack.value;
+      
+      // coursepacks array population
+      requestObj.coursepack = [];
+      var v_aCoursepacks = $('#coursepacksArray .form-control');
+      for(var i=0; i < v_aCoursepacks.length; i++)
+      {
+        requestObj.coursepack[i] = v_aCoursepacks[i].value;
+      }
 
-      Meteor.call("createRequest", requestObj);
+      if( isFormValid(requestObj) )
+      {
+        Meteor.call("createRequest", requestObj);
+        $("#sendRequest-panel").addClass("hidden");
 
-      e.target.firstName.value; 
-      e.target.lastName.value; 
-      e.target.email.value; 
-      e.target.studentNumber.value; 
-      e.target.coursepack.value;
+        var v_sThankYou = "Thank you " + requestObj.firstName +  " for requesting a coursepack. " +
+                        " IEEE Carleton will contact you shortly to purchase your requested coursepacks";
+        $("#confirmationMessage").html( v_sThankYou );       
+        $("#confirmationMessage").removeClass("hidden");
+        $("#validationError").addClass("hidden");
+      }
+      else
+      {
+        $("#validationError").removeClass("hidden");
+      }
     }
   });
 
@@ -48,8 +61,67 @@
       Meteor.call("deleteCoursepack", this._id);
     }
   });
+
   Template.request.events({
     "click .delete": function () {
       Meteor.call("deleteRequest", this._id);
     }
   });
+
+  Template.exportRequests.events({
+    'click .export-data': function() { 
+      var requestsRecords =  Requests.find({}, {sort: {createdAt: -1}});
+
+      var csvDocument = "";
+      csvDocument += "Name, Student Number, Email, Coursepack, Date of Request\r\n";
+
+      requestsRecords.forEach(function(rec) {
+        csvDocument += rec.firstName + " " + rec.lastName + "," + 
+                       rec.studentNumber + "," + rec.email + ", " + 
+                       rec.coursepack + "," + rec.createdAt + "\r\n";
+      });
+
+      var dlLink = document.createElement('a');
+      var date = new Date();
+      dlLink.download = "Requests_ " + date + ".csv";
+      dlLink.href = "data:application/csv," + escape(csvDocument);
+      dlLink.click();
+    }
+  });
+
+
+function isFormValid(o)
+{
+    if( o.firstName === "" ) { return false; }
+    if( o.lastName === "") { return false; }
+    if( o.email === "") { return false; }
+    if( !o.email.includes("@cmail.carleton.ca") ) { return false; }
+    if( o.studentNumber.length !== 9) { return false; }
+
+    return true;
+}
+
+$(function()
+{
+    $(document).on('click', '.btn-add', function(e)
+    {
+        e.preventDefault();
+
+        var controlForm = $('.controls form:first'),
+            currentEntry = $(this).parents('.entry:first'),
+            newEntry = $(currentEntry.clone()).appendTo(controlForm);
+
+        newEntry.find('input').val('');
+        controlForm.find('.entry:not(:last) .btn-add')
+            .removeClass('btn-add').addClass('btn-remove')
+            .removeClass('btn-success').addClass('btn-danger')
+            .html('<span class="glyphicon glyphicon-minus"></span>');
+    }).on('click', '.btn-remove', function(e)
+    {
+    $(this).parents('.entry:first').remove();
+
+    e.preventDefault();
+    return false;
+  });
+});
+    
--- client/lib/environments.js
+++ client/lib/environments.js
@@ -6,6 +6,28 @@
     this.render('admin');
   });
 
+  Router.route('/csv', {
+    where: 'server',
+    action: function () {
+      var filename = 'requets.csv';
+      var fileData = "";
+
+      var headers = {
+        'Content-type': 'text/csv',
+        'Content-Disposition': "attachment; filename=" + filename
+      };
+      var records = Requests.find();
+      // build a CSV string. Oversimplified. You'd have to escape quotes and commas.
+      records.forEach(function(rec) {
+        fileData += rec.firstName + " " + rec.lastName + "," + rec.email + "," + rec.studentNumber + "," + rec.coursepack + "\r\n";
+      });
+      this.response.writeHead(200, headers);
+      return this.response.end(fileData);
+    }
+  });
+
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
+
+
